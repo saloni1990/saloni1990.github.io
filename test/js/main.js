22df5fd947a65155a2b9e18750518a1d5399d318
saloni1990 <salonicmd@gmail.com>
2018-02-18 17:36:58 +0000

    Update main.js

--- test/js/main.js
+++ test/js/main.js
@@ -4,9 +4,12 @@ alert(skillset);*/
 function main() {
   $('.skillset').hide();
   $('.skillset').fadeIn(1000);
+  
   $('.projects').hide();
+  
   $('.projects-button').on('click', function() {
-   $('.projects').toggle()
-  });
+		$('.projects').toggle();
+    $(this).toggleClass('active');
+	});
 }
 $(document).ready(main);
