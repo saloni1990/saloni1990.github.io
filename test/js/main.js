ec40baa423821153a4021c2b1eee6812d1f5333c
saloni1990 <salonicmd@gmail.com>
2018-02-18 19:46:44 +0000

    Update main.js

--- test/js/main.js
+++ test/js/main.js
@@ -8,8 +8,12 @@ function main() {
   $('.projects').hide();
   
   $('.projects-button').on('click', function() {
-    $('.projects').toggle();
+		/* this like can prevent the slideToggle from working properly, since it is also affecting the showing and hiding behavior of the projects element
+    $(this).next().toggle();
+    */
+    $(this).next().slideToggle(400);
     $(this).toggleClass('active');
+    $(this).text('Projects Viewed')
 	});
 }
 $(document).ready(main);
