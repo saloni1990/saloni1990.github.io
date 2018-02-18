32502717e744695a48ec88635e5786cd44e266f0
saloni1990 <salonicmd@gmail.com>
2018-02-18 11:58:35 +0000

    Update main.js

--- test/js/main.js
+++ test/js/main.js
@@ -1,6 +1,10 @@
 /*var skillset = document.getElementsByClassName('skillset');
 
 alert(skillset);*/
+function() {
+		$("#header").load("includes/header.html");
+		$("#footer").load("includes/footer.html");
+};
 function main() {
 $('.skillset').hide();
 $('.skillset').fadeIn(1000);
