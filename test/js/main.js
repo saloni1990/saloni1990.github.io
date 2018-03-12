12cba956f73833895af804e06ed4281c010ed6e4
saloni1990 <salonicmd@gmail.com>
2018-03-12 20:28:15 +0000

    updated

--- test/js/main.js
+++ test/js/main.js
@@ -5,15 +5,15 @@ function main() {
   $('.skillset').hide();
   $('.skillset').fadeIn(1000);
   
-  $('.projects').hide();
+  $('.resources').hide();
   
-  $('.projects-button').on('click', function() {
+  $('.resources-button').on('click', function() {
 		/* this like can prevent the slideToggle from working properly, since it is also affecting the showing and hiding behavior of the projects element
     $(this).next().toggle();
     */
     $(this).next().slideToggle(400);
     $(this).toggleClass('active');
-    $(this).text('Courses Viewed')
+    $(this).text('Viewed')
 	});
 }
 
